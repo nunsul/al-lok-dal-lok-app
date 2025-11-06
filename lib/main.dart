6da890850e2f267931c7d9ad3f8ad1a70f332ff5
nunsul <younghunk17@naver.com>
2025-11-06 13:28:52 +0900

    요거는 파이어베이스 어스가 로그인유저인지 감지!
    화면분기 이상해서 고쳤습니다 정상 작동!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:math_demo/login/Main_Login_Screen.dart';
+import 'package:math_demo/screen/Main_back_screen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -11,14 +13,27 @@ Future<void> main() async {
   runApp(const MathRunApp());
 }
 
-class MathRunApp extends StatelessWidget {
+class MathRunApp extends StatefulWidget {
   const MathRunApp({super.key});
-
+  @override
+  State<MathRunApp> createState() => _MathRunAppState();
+}
+class _MathRunAppState extends State<MathRunApp> {
+  String? user;
+  @override
+  void initState(){
+    super.initState();
+    _loadMe();
+  }
+  Future<void> _loadMe() async{
+    final db = FirebaseAuth.instance;
+    user = db.currentUser?.uid;
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Main_Login_Screen(),
+      home: (user==null) ? Main_Login_Screen() : MainBackScreen(),
     );
   }
 }
