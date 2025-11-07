9a9cd322300d21a19d0995313ea91af7088d1bae
nunsul <younghunk17@naver.com>
2025-11-07 16:14:10 +0900

    git commit -m chore: Firebase yami에 추가
    현재 매인 자체에 파이어베이스가 인식이 되지 않아서 수정했으며, 이미지 및 로고가 없어서 추가했습니다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Firebase Core import
-import 'firebase_options.dart'; // 자동 생성된 설정 파일
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
